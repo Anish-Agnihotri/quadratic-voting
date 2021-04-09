723d90fed7f3efd317efb03f2d528f963913119b
alexrandaccio <acrandaccio@gmail.com>
2021-04-09 13:53:15 -0400

    prevent new db connections on hot reload

--- db/index.js
+++ db/index.js
@@ -1,7 +1,20 @@
 const { PrismaClient } = require("@prisma/client"); // Import prisma client
 
+let prisma;
+
 // Setup prisma client
-const prisma = new PrismaClient();
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient()
+} else {
+  // if running in dev mode, make sure hot reloads don't open a new connection
+  if (!global.prisma) {
+    console.log("creating new prisma client")
+    global.prisma = new PrismaClient()
+  }
+
+  prisma = global.prisma
+}
+
 
 // Export prisma client
 export default prisma;
